d74fa0a846eddda33c5f88230cb05d1de0ab6fbd
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-04-30 11:10:55 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// Copyright 2022 Dacyto
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+#include "textgen.h"
+
+int main() {
+    MarkovGenerator gen;
+
+    std::ifstream file("input.txt");
+    Prefix start = gen.ReadState(file);
+
+    unsigned state = time(nullptr);
+
+    gen.write(std::cout, start, &state);
+}
